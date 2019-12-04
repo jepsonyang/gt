6743bbe74af18cdace66f9ca0b7ac5a63ce91293
jepsonyang <jepsonyang@tencent.com>
2019-12-04 21:12:11 +0800

    random

--- random/random.go
+++ random/random.go
@@ -14,3 +14,13 @@ func RandomScope(min int, max int) int {
 	random := rand.New(seed)
 	return min + random.Intn(max-min)
 }
+
+/*
+* 生成指定范围内的随机数
+* @return 生成的随机数范围[min, max)
+**/
+func RandomScopeInt64(min int64, max int64) int64 {
+	seed := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(seed)
+	return min + random.Int63n(max-min)
+}
