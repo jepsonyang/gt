44be9d62b067240cae47a7c71838cca2c62bfba0
jepsonyang <jepsonyang@tencent.com>
2019-12-07 12:46:09 +0800

    注释优化

--- gtRedis/hash.go
+++ gtRedis/hash.go
@@ -7,7 +7,7 @@ import (
 /*
 * 设置hash值
 * @value 可以传入map[string]interface{}或者结构体对象
-* @note 传入map[string]interface{}时，只会更新(或者覆盖)map包含的字段，其他字段的值不会被改变;
+* @note 传入map[string]interface{}时，只会更新(或者覆盖)map包含的字段，其他字段的值不会被改变;也可以通过指定结构体字段tag标记"omitempty",来忽略空值的字段;
 **/
 func HashSet(conn redis.Conn, key string, value interface{}) error {
 	args := redis.Args{}
@@ -20,7 +20,7 @@ func HashSet(conn redis.Conn, key string, value interface{}) error {
 /*
 * 获取hash值
 * @dst 必须传入结构体对象指针
-* @note 如果key不存在，不报错，dst的的值不变;
+* @note 如果key不存在,不报错,dst的的值不变;如果key存在,redis hash中有的字段,会对应更新到dst结构体的相应字段,hash中不存在的字段，dst中对应字段值保持不变;
 **/
 func HashGet(conn redis.Conn, key string, dst interface{}) error {
 	arrReply, err := redis.Values(conn.Do("HGETALL", key))
