d01be12c9c334bf059ec2bab0c9cc00dba9ebb97
jepsonyang <jepsonyang@tencent.com>
2019-12-20 20:37:25 +0800

    增加RandomString

--- gtRandom/random.go
+++ gtRandom/random.go
@@ -6,6 +6,22 @@ import (
 	"time"
 )
 
+const (
+	KCharSetNum         = 1
+	KCharSetUpperLetter = 1 << 1
+	KCharSetLowLetter   = 1 << 2
+)
+
+var counter int64
+var mutex sync.Mutex
+
+func count() int64 {
+	mutex.Lock()
+	defer mutex.Unlock()
+	counter++
+	return counter
+}
+
 /*
 * 生成指定范围内的随机数
 * @return 生成的随机数范围[min, max)
@@ -26,12 +42,44 @@ func RandomScopeInt64(min int64, max int64) int64 {
 	return min + random.Int63n(max-min)
 }
 
-var counter int64
-var mutex sync.Mutex
+func RandomString(length int, charSet int) string {
+	if length <= 0 {
+		return ""
+	}
 
-func count() int64 {
-	mutex.Lock()
-	defer mutex.Unlock()
-	counter++
-	return counter
+	numbers := []byte("0123456789")
+	upperLetters := []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	lowLetters := []byte("abcdefghijklmnopqrstuvwxyz")
+
+	charPool := make([]byte, 0)
+
+	//数字
+	if charSet&KCharSetNum == 1 {
+		charPool = append(charPool, numbers...)
+	}
+
+	//大写字母
+	if charSet&KCharSetUpperLetter == 1 {
+		charPool = append(charPool, upperLetters...)
+	}
+
+	//小写字母
+	if charSet&KCharSetLowLetter == 1 {
+		charPool = append(charPool, lowLetters...)
+	}
+
+	lenPool := len(charPool)
+	if lenPool <= 0 {
+		return ""
+	}
+
+	seed := rand.NewSource(time.Now().UnixNano() + count())
+	random := rand.New(seed)
+
+	result := make([]byte, length)
+	for i := 0; i < length; i++ {
+		result[i] = charPool[random.Intn(lenPool)]
+	}
+
+	return string(result)
 }
