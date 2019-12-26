2a92577f3dfabdbbbf168b990652c22c4c2f2e56
jepsonyang <jepsonyang@tencent.com>
2019-12-26 17:00:43 +0800

    注释优化

--- gt/utils.go
+++ gt/utils.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 )
 
+/*
+* 将map[string]interface{}转成struct
+* @dst 必须传入结构体对象指针
+**/
 func MapStr2Struct(src map[string]interface{}, dst interface{}) error {
 	dataBytes, err := json.Marshal(src)
 	if err != nil {
