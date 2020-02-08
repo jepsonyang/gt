f844ada8ab0e16c8a041db789cf309ff615daa74
jepsonyang <jepsonyang@tencent.com>
2020-02-08 12:29:34 +0800

    增加GetGID()接口

--- gt/utils.go
+++ gt/utils.go
@@ -1,8 +1,11 @@
 package gt
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"runtime"
+	"strconv"
 )
 
 /*
@@ -19,4 +22,17 @@ func MapStr2Struct(src map[string]interface{}, dst interface{}) error {
 		return fmt.Errorf("Unmarshal failed. err: %v ", err)
 	}
 	return nil
+}
+
+/*
+* 获取协程id
+* @note go从1.4版本开始，去掉了获取协程id的接口;此方法通过获取调用堆栈，从而提取协程id，仅可用于对性能不敏感的场景
+**/
+func GetGID() uint64 {
+	buffer := make([]byte, 64)
+	buffer = buffer[:runtime.Stack(buffer, false)]
+	buffer = bytes.TrimPrefix(buffer, []byte("goroutine "))
+	buffer = buffer[:bytes.IndexByte(buffer, ' ')]
+	gid, _ := strconv.ParseUint(string(buffer), 10, 64)
+	return gid
 }
