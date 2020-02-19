39341dbda8a6268b585f36b093964a60747bd237
jepsonyang <jepsonyang@tencent.com>
2020-02-19 18:13:00 +0800

    接口增加注释

--- gtNet/request.go
+++ gtNet/request.go
@@ -6,10 +6,20 @@ import (
 	"net/http"
 )
 
+/*
+* POST请求
+* @headers http请求头;可以使用NewJsonHeader()生成json请求头;
+* @client 允许传入自定义的Client对象;传入nil，每次请求将会临时生成默认Client对象;
+**/
 func Post(url string, headers map[string]string, bodyByte []byte, client *http.Client) ([]byte, error) {
 	return request(url, headers, bodyByte, http.MethodPost, client)
 }
 
+/*
+* GET请求
+* @headers http请求头;可以使用NewJsonHeader()生成json请求头;
+* @client 允许传入自定义的Client对象;传入nil，每次请求将会临时生成默认Client对象;
+**/
 func Get(url string, headers map[string]string, client *http.Client) ([]byte, error) {
 	return request(url, headers, []byte(""), http.MethodGet, client)
 }
